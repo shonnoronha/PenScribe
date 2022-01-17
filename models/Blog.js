2d1596eb312e8c768adba64bda43e86f6d6beb1f
shonnoronha <shon.noronha@gmail.com>
2022-01-17 16:31:43 +0530

    filter bad words

--- models/Blog.js
+++ models/Blog.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const User = require('./User');
+const Filter = require('bad-words');
+const filter = new Filter();
 
 const blogSchema = new Schema({
     title: {
@@ -27,6 +29,13 @@ const blogSchema = new Schema({
     }
 }, {
     timestamps : true,
+});
+
+blogSchema.pre('save', function(next){
+    this.title = filter.clean(this.title);
+    this.description = filter.clean(this.description);
+    this.content = filter.clean(this.content);
+    next();
 });
 
 const Blog = mongoose.model('blog', blogSchema);
